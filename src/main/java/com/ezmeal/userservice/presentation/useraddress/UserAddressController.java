460ee14ed11a1c2e39671579a084e5d9ac1358bf
SeongWon Jeong <newon.jsw@gmail.com>
2026-04-26 22:44:39 +0900

    chore: add controller files

---
+++ src/main/java/com/ezmeal/userservice/presentation/useraddress/UserAddressController.java
@@ -0,0 +1,9 @@
+package com.ezmeal.userservice.presentation.useraddress;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users/addresses")
+public class UserAddressController {
+}
