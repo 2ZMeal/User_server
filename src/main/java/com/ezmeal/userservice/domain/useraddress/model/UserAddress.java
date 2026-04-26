530ac5cecd5933e1bbff27b43741b82a4cad75d5
SeongWon Jeong <newon.jsw@gmail.com>
2026-04-26 21:42:12 +0900

    feat: add user address entity

---
+++ src/main/java/com/ezmeal/userservice/domain/useraddress/model/UserAddress.java
@@ -0,0 +1,53 @@
+package com.ezmeal.userservice.domain.useraddress.model;
+
+import com.ezmeal.userservice.domain.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_user_address", schema = "user_db")
+// TODO : extends Base Entity
+public class UserAddress {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(name="id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", updatable = false, nullable = false)
+    private User user;
+
+    @Column(name="road_name_address", nullable = false)
+    private String roadNameAddress;
+
+    @Column(name="lot_number_address", nullable = false)
+    private String lotNumberAddress;
+
+    @Column(name="zip_code", nullable = false)
+    private String zipCode;
+
+    @Column(name="is_default", nullable = false)
+    private boolean isDefault;
+
+    // Factory Methods ====================================================
+    public static UserAddress create() {
+        // TODO : Complete after extends BaseEntity
+        UserAddress userAddress = new UserAddress();
+        return userAddress;
+    }
+}
