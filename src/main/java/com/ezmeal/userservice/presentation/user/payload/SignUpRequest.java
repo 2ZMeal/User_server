88ab48945d363d0ea7112bca252689c1c161b57d
SeongWon Jeong <newon.jsw@gmail.com>
2026-05-03 20:42:09 +0900

    refactor: add validation failure message

--- src/main/java/com/ezmeal/userservice/presentation/user/payload/SignUpRequest.java
+++ src/main/java/com/ezmeal/userservice/presentation/user/payload/SignUpRequest.java
@@ -9,23 +9,23 @@ import jakarta.validation.constraints.Size;
 
 public record SignUpRequest(
 
-    @Email
-    @NotBlank
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    @NotBlank(message = "이메일은 필수입니다.")
     @Size(min = 1, max = 30)
     String email,
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수입니다.(8~30자)")
     @Size(min=8, max = 30)
     String password,
 
-    @Size(max=20)
+    @Size(max=20, message = "닉네임은 20자 이하로 작성해주세요.")
     String nickname,
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수입니다.")
     @Size(max=10)
     String name,
 
-    @NotNull
+    @NotNull(message = "Role must be not null")
     Role role
 ) {
 
